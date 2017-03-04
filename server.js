5bd64b8c1c16a1465d09595ceded473edf481e89
Sim Gill <simgill89@gmail.com>
2017-03-03 20:50:59 -0800

    heroku

--- server.js
+++ server.js
@@ -1,30 +1,97 @@
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+// const express = require('express');
+// const app = express();
+// app.use(bodyParser.json());
+// const path = require('path');
+// app.use('/',express.static(path.join(__dirname, 'client')));
+
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+    host : 'postgres://dyywuotk:7TvX8RVk9BrOZdAwDGdc32WaUu2pQSuh@babar.elephantsql.com:5432/dyywuotk',
+    user : 'dyywuotk',
+    password : '7TvX8RVk9BrOZdAwDGdc32WaUu2pQSuh',
+    database : 'dyywuotk'
+  },
+});
+
+
+
+
+
+
 const app = express();
+
 app.use(bodyParser.json());
-const path = require('path');
-app.use('/',express.static(path.join(__dirname, 'client')));
 
+app.post('/recipes', (req, res) => {
+    const requiredFields = ['name', 'description', 'steps'];
 
+    const missingIndex = requiredFields.findIndex(field => !req.body[field]);
+    if (missingIndex != -1) {
+        return res.status(400).json({
+            message: `Missing field: ${requiredFields[missingIndex]}`
+        });
+    }
 
+    const recipe = {
+        name: req.body.name,
+        description: req.body.description
+    };
 
+    knex
+        .insert(recipe)
+        .into('recipes')
+        .returning('id')
+        .then(ids => {
+            const recipeId = ids[0];
+            const steps = req.body.steps.map(step => ({
+                content: step,
+                recipe_id: recipeId
+            }));
 
-app.get('/post', (req, res) => {})
+            recipe.id = recipeId;
 
+            return knex.insert(steps).into('steps');
+        })
+        .then(() => {
+            res.status(201).json({});
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                message: 'Internal server error'
+            });
+        });
+});
 
-app.get('/post/:id', (req, res) => {});
+app.get('/recipes', (req, res) => {
+    knex
+        .select('recipes.id', 'recipes.name', 'recipes.description', 'steps.content')
+        .from('recipes')
+        .leftOuterJoin('steps', 'recipes.id', 'steps.recipe_id')
+        .then(rows => {
+            const merged = {};
+            rows.forEach(row => {
+                if (!(row.id in merged)) {
+                    merged[row.id] = {
+                        id: row.id,
+                        name: row.name,
+                        description: row.description,
+                        steps: row.content ? [row.content] : []
+                    }
+                    return;
+                }
+                merged[row.id].steps.push(row.content);
+            });
+            res.json(Object.keys(merged).map(key => merged[key]));
+        });
+});
 
-app.post('/post', (req, res) => {})
-
-app.put('/post/:id', (req, res) => {})
-
-app.delete('/post/:id', (req , res) =>{});
-
-
-
-
-
-
+app.listen(process.env.PORT || 8080);
 
 
 
