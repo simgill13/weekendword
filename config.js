c0703e3191e3d28612cb7b0fe5d3038c86f3bb4f
Sim Gill <simgill89@gmail.com>
2017-03-03 21:14:51 -0800

    heroku

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
-// exports.DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL ||
-                      
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL;
+                      // 'mongodb://test:password@ds161039.mlab.com:61039/fullstackdb';
                        
-// exports.PORT = process.env.PORT || 8080;
-
-
-//not using this code
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,90 +1,67 @@
-const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const app = express();
 app.use(bodyParser.json());
 const path = require('path');
+const {blogPost} = require('./model');
+const {PORT, DATABASE_URL} = require('./config');
 app.use('/',express.static(path.join(__dirname, 'client')));
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//     host : 'postgres://dyywuotk:7TvX8RVk9BrOZdAwDGdc32WaUu2pQSuh@babar.elephantsql.com:5432/dyywuotk',
-//     user : 'dyywuotk',
-//     password : '7TvX8RVk9BrOZdAwDGdc32WaUu2pQSuh',
-//     database : 'dyywuotk'
-//   },
-// });
+
+
+
+app.get('/post', (req, res) => {
+   
+  res.sendStatus(200);     	
+})
 
 
 
 
 
 
+let server;
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 
-// app.post('/recipes', (req, res) => {
-//     const requiredFields = ['name', 'description', 'steps'];
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
 
-//     const missingIndex = requiredFields.findIndex(field => !req.body[field]);
-//     if (missingIndex != -1) {
-//         return res.status(400).json({
-//             message: `Missing field: ${requiredFields[missingIndex]}`
-//         });
-//     }
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
 
-//     const recipe = {
-//         name: req.body.name,
-//         description: req.body.description
-//     };
 
-//     knex
-//         .insert(recipe)
-//         .into('recipes')
-//         .returning('id')
-//         .then(ids => {
-//             const recipeId = ids[0];
-//             const steps = req.body.steps.map(step => ({
-//                 content: step,
-//                 recipe_id: recipeId
-//             }));
 
-//             recipe.id = recipeId;
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
 
-//             return knex.insert(steps).into('steps');
-//         })
-//         .then(() => {
-//             res.status(201).json({});
-//         })
-//         .catch(err => {
-//             console.error(err);
-//             res.status(500).json({
-//                 message: 'Internal server error'
-//             });
-//         });
-// });
-
-// app.get('/recipes', (req, res) => {
-//     knex
-//         .select('recipes.id', 'recipes.name', 'recipes.description', 'steps.content')
-//         .from('recipes')
-//         .leftOuterJoin('steps', 'recipes.id', 'steps.recipe_id')
-//         .then(rows => {
-//             const merged = {};
-//             rows.forEach(row => {
-//                 if (!(row.id in merged)) {
-//                     merged[row.id] = {
-//                         id: row.id,
-//                         name: row.name,
-//                         description: row.description,
-//                         steps: row.content ? [row.content] : []
-//                     }
-//                     return;
-//                 }
-//                 merged[row.id].steps.push(row.content);
-//             });
-//             res.json(Object.keys(merged).map(key => merged[key]));
-//         });
-// });
-
-// app.listen(process.env.PORT || 8080);
+module.exports = {app, runServer, closeServer};
 
 
 
@@ -99,5 +76,7 @@ app.use('/',express.static(path.join(__dirname, 'client')));
 
 
 
-// app.listen(process.env.PORT || 8080, () => console.log(
-//   `Your app is listening on port ${process.env.PORT || 8080 }`));
+
+
+
+
