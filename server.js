74c9881fb37ba41ade7b863128dc977c7454fe06
Sim Gill <simgill89@gmail.com>
2017-03-03 21:57:21 -0800

    d

--- server.js
+++ server.js
@@ -4,15 +4,57 @@ const app = express();
 // const bodyParser = require('body-parser');
 const path = require('path');
 app.use('/',express.static(path.join(__dirname, 'client')));
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//     host : 'postgres://dyywuotk:7TvX8RVk9BrOZdAwDGdc32WaUu2pQSuh@babar.elephantsql.com:5432/dyywuotk',
-//     user : 'dyywuotk',
-//     password : '7TvX8RVk9BrOZdAwDGdc32WaUu2pQSuh',
-//     database : 'dyywuotk'
-//   },
-// });
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+    host : 'ec2-184-72-252-69.compute-1.amazonaws.com',
+    user : 'ialeurvhndzsio',
+    password : 'e2016813fd4c28db4b657859fdfbcd15bc8039c67dda63b52987a40efa4aca4b',
+    database : 'd4mcuuro9s7ol0'
+  },
+});
+
+
+
+
+
+
+app.get('/test', (req,res) => {
+    knex.schema.createTableIfNotExists("test", function (table) {
+        table.increments(); 
+        table.string('name');
+        table.string('description');
+    })
+    .then(data => {
+    	console.log(data);
+    	res.status(200).json({message: "hope"});
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
