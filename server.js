6ee641c94b523ccc8e1adc50de46a9b27a73c7b1
Sim Gill <simgill89@gmail.com>
2017-03-03 21:37:50 -0800

    d

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use('/',express.static(path.join(__dirname, 'client')));
 
 
 
-// app.listen(process.env.PORT || 8080, () => console.log(
-//   `Your app is listening on port ${process.env.PORT || 8080 }`));
+app.listen(process.env.PORT || 8080, () => console.log(
+  `Your app is listening on port ${process.env.PORT || 8080 }`));
